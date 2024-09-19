20596c3748e035cfec43d4e74c3cfc91c204d1c2
slammokl <root@WIN-63QRHS43TS5>
2024-09-19 08:19:07 +0300

    First commit

---
+++ autumn/task1.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include "R2Graph.h"
+
+int main() {
+	R2Point a; 
+	R2Point b;
+	R2Point c;
+	std::cout << "Введите вершины треугольника: " << std::endl;
+	std::cin >> a;
+	std::cin >> b;
+	std::cin >> c;
+	return 0;
+}
